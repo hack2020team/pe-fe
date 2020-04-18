533a9f610dffbd767965162d93a5ca067a68338e
Ewelina Gromada <gromada.ewelina@gmail.com>
2020-04-18 15:33:31 +0200

    charts

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Table, Typography, TableBody,TableCell,TableContainer, TableHead, TableRow, LinearProgress, Dialog, DialogActions, DialogContentText, DialogContent, DialogTitle, Button, TextField } from '@material-ui/core';
+import { Table, Paper, Grid, Typography, TableBody,TableCell,TableContainer, TableHead, TableRow, LinearProgress, Dialog, DialogActions, DialogContentText, DialogContent, DialogTitle, Button, TextField } from '@material-ui/core';
 
 const useStyles = makeStyles({
   table: {
@@ -21,76 +21,51 @@ const courses = [
 
 export default function SimpleTable() {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  
-  const handleClose = () => {
-    setOpen(false);
-  };
+
   return (
     <div>
-    <TableContainer>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Courses</TableCell>
-            <TableCell align="left">Progress</TableCell>
-            <TableCell align="right">Skills</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {courses.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-              <Typography variant="subtitle1" style={{fontWeight: 'bold'}} gutterBottom>
-                {row.name}
-              </Typography> 
-              </TableCell>
-              <TableCell align="right">
-              <div className={classes.root}>
-                <LinearProgress style={{height: "15px"}} color="secondary" variant="determinate" value={row.progress} />
-              </div>
-              </TableCell>
-              <TableCell align="right"><img src={row.img} width="90px"/></TableCell>
-            </TableRow>
-          ))}
-           {/* <TableRow>
-              <TableCell component="th" scope="row">
-                <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                  + Add course
-                </Button>
-                
-              </TableCell>
-            </TableRow> */}
-        </TableBody>
-       
-      </Table>
-    </TableContainer>
-     <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-     <DialogTitle id="form-dialog-title">Add course</DialogTitle>
-       <DialogContent>
-         <DialogContentText>
-          Add name of the new course
-         </DialogContentText>
-         <TextField
-           autoFocus
-           margin="dense"
-           id="name"
-           label="Course"
-           fullWidth
-         />
-       </DialogContent>
-       <DialogActions>
-         <Button onClick={handleClose} color="primary">
-           Cancel
-         </Button>
-         <Button onClick={handleClose} color="primary">
-           Add
-         </Button>
-       </DialogActions>
-   </Dialog>
+    <Grid container spacing={3}>
+      <Grid item xs={8}>
+        <TableContainer>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Courses</TableCell>
+                <TableCell align="left">Progress</TableCell>
+                <TableCell align="right">Skills</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {courses.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                  <Typography variant="subtitle1" style={{fontWeight: 'bold'}} gutterBottom>
+                    {row.name}
+                  </Typography> 
+                  </TableCell>
+                  <TableCell align="right">
+                  <div className={classes.root}>
+                    <LinearProgress style={{height: "15px"}} color="secondary" variant="determinate" value={row.progress} />
+                  </div>
+                  </TableCell>
+                  <TableCell align="right"><img src={row.img} width="90px"/></TableCell>
+                </TableRow>
+              ))}
+              
+            </TableBody>
+          </Table>
+        </TableContainer>
+        </Grid>
+        <Grid item xs={4}>
+          <div style={{textAlign: 'center'}}>
+            <img  width="270px" src="https://youlearn.s3.eu-central-1.amazonaws.com/math/skills-chart/Screenshot+2020-04-18+at+15.14.40.png" />
+            <img  width="400px" style={{paddingTop: '50px'}} src="https://youlearn.s3.eu-central-1.amazonaws.com/math/skills-chart/Screenshot+2020-04-18+at+15.27.49.png" />
+
+          </div>
+        </Grid>
+      
+        </Grid>
+    
    </div>
   );
 }
